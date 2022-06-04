7b2c387f14339d32b6e3e98bbc84093c54572c8d
naveen <naveenrd@tekion.com>
2022-06-05 03:34:43 +0530

    updated property file Reading with static block initialization

--- src/main/java/com/example/utils/readPropertyFile.java
+++ src/main/java/com/example/utils/readPropertyFile.java
@@ -1,7 +1,10 @@
 package com.example.utils;
 
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Objects;
 import java.util.Properties;
 
@@ -10,18 +13,42 @@ public final class readPropertyFile {
 
     private readPropertyFile(){}
 
+    private static Properties properties = new Properties();
+    private static final Map<String,String> CONFIGMAP = new HashMap<>();
+
+    static {
+        try {
+            FileInputStream file = new FileInputStream(System.getProperty("user.dir") + "/src/test/resources/configs/config.properties");
+            properties.load(file);
+
+            for( Map.Entry<Object, Object> entry:properties.entrySet())
+                CONFIGMAP.put((String) entry.getKey(), (String) entry.getValue());
+
+        }
+        catch (FileNotFoundException e)
+        {
+            e.printStackTrace();
+        }
+        catch (IOException e)
+        {
+            e.printStackTrace();
+        }
+    }
+
+
+
     public static String getValue (String Key) throws Exception {
         String value = "";
-        Properties properties = new Properties();
-        FileInputStream file = new FileInputStream( System.getProperty("user.dir")+"/src/test/resources/configs/config.properties");
-        properties.load(file);
 
-        if(Objects.isNull(properties.getProperty(Key)))
+        if(Objects.isNull(Key))
         {
-            throw new Exception("Property Name "+Key+" is not found");
+            throw new Exception("Property Name "+Key+" is null. Please check the calling method arugument");
         }
-
-        return properties.getProperty(Key);
+        else if(Objects.isNull(properties.getProperty(Key)))
+        {
+            throw new Exception("Property Name "+Key+" is not found. Please check the property file");
+        }
+        return CONFIGMAP.get(Key);
     }
 
 }
